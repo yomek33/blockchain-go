4b47f1b64d985a8a19ac110b2c95a17cf1928ba8
yomek33 <i3landme@gmail.com>
2024-11-18 21:04:41 +0900

    refactor

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"bytes"
-	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
 	"log"
-	"math"
-	"math/big"
 	"strconv"
 	"time"
 )
 
 func main() {
+	start := time.Now()
 	bc := NewBlockChain()
 
 	bc.AddBlock("Send 1 BTC to Ivan")
@@ -22,108 +20,17 @@ func main() {
 		fmt.Printf("Prev.Hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Printf("Elapsed time: %s\n", elapsed)
 }
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-func (bs *BlockChain) AddBlock(data string) {
-	prevBlock := bs.blocks[len(bs.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bs.blocks = append(bs.blocks, newBlock)
-}
-
-func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{NewGenesisBlock()}}
-}
-
-type Block struct {
-	Timestamp     int64
-	Data          []byte
-	PrevBlockHash []byte
-	Hash          []byte
-}
-
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
-}
-
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
-	return block
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
-// proof of work
-const targetBits = 24 //任意の数字
-var maxNonce = math.MaxInt64
-
-type ProofOfWork struct {
-	block  *Block
-	target *big.Int
-}
-
-func NewProofOfWork(b *Block) *ProofOfWork {
-	target := big.NewInt(1)
-	target.Lsh(target, uint(256-targetBits))
-
-	pow := &ProofOfWork{b, target}
-	return pow
-}
-
-func (pow *ProofOfWork) prepareData(nonce int) []byte {
-	data := bytes.Join(
-		[][]byte{
-			pow.block.PrevBlockHash,
-			pow.block.Data,
-			IntToHex(pow.block.Timestamp),
-			IntToHex(int64(targetBits)),
-			IntToHex(int64(nonce)),
-		},
-		[]byte{},
-	)
-
-	return data
-}
-
-func (pow *ProofOfWork) Run() (int, []byte) {
-	var hashInt big.Int
-	var hash [32]byte
-	nonce := 0
-
-	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
-
-	for nonce < maxNonce {
-		date := pow.prepareData(nonce)
-		hash = sha256.Sum256(date)
-
-		fmt.Printf("\r%x", hash)
-
-		hashInt.SetBytes(hash[:])
-
-		if hashInt.Cmp(pow.target) == -1 {
-			break
-		} else {
-			nonce++
-		}
-
-	}
-
-	fmt.Print("\n\n")
-	return nonce, hash[:]
-}
-
+// util
 // convert an int64 to a byte array
 func IntToHex(num int64) []byte {
 	buff := new(bytes.Buffer)
